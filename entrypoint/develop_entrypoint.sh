ed758c1e7850db6650c261a5149fc903af636a0c
Dimitrios Desyllas <pc-magas@users.noreply.github.com>
2019-06-14 12:10:10 +0300

    Use Entrypoint and make XDEBUG Settings

--- entrypoint/develop_entrypoint.sh
+++ entrypoint/develop_entrypoint.sh
@@ -3,7 +3,7 @@
 USER_ID=$(id -u developer)
 GROUP_ID=$(id -g developer)
 
-echo "Setting the correct user and group id for shell use"
+echo "Setting the correct user and group id for shell use \n"
 if [ ${DOCKER_UID} != ${USER_ID} ]; then
   usermod -u ${DOCKER_UID} developer
 fi
@@ -12,4 +12,24 @@ if [ ${DOCKER_GID} != ${GROUP_ID} ]; then
   groupmod -g ${DOCKER_GID} developer
 fi
 
-echo "Setup xdebug"
+echo "Setup xdebug\n"
+cp ${XDEBUG_CONF_FILE}.orig ${XDEBUG_CONF_FILE}
+
+if [ -z ${XDEBUG_HOST} ]; then
+    ip=$(netstat -rn | grep "^0.0.0.0 " | cut -d " " -f10)
+    XDEBUG_HOST=${ip}
+fi
+
+echo "xdebug.remote_host=${XDEBUG_HOST}" >> ${XDEBUG_CONF_FILE}
+echo "xdebug.repomote_port=${XDEBUG_PORT}" >> ${XDEBUG_CONF_FILE}
+
+if [XDEBUG_DBGP = TRUE]; then
+    echo "xdebug.remote.handler=dbgp" >> ${XDEBUG_CONF_FILE}
+fi
+
+if [! -z "${XDEBUG_IDE_KEY}"]; then
+    echo "xdebug.idekey=\"${XDEBUG_IDE_KEY}\"" >> ${XDEBUG_CONF_FILE}
+fi
+
+echo "Launch application\n"
+exec "$@"
