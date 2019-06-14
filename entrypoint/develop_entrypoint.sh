f09eb1d6c2cb81ff771653c55e2bdc23ad7e44f0
Dimitrios Desyllas <pc-magas@users.noreply.github.com>
2019-06-14 11:28:33 +0300

    ETABLE-654 XDEBUG Configuration

--- entrypoint/develop_entrypoint.sh
+++ entrypoint/develop_entrypoint.sh
@@ -3,6 +3,7 @@
 USER_ID=$(id -u developer)
 GROUP_ID=$(id -g developer)
 
+echo "Setting the correct user and group id for shell use"
 if [ ${DOCKER_UID} != ${USER_ID} ]; then
   usermod -u ${DOCKER_UID} developer
 fi
@@ -10,3 +11,5 @@ fi
 if [ ${DOCKER_GID} != ${GROUP_ID} ]; then
   groupmod -g ${DOCKER_GID} developer
 fi
+
+echo "Setup xdebug"
