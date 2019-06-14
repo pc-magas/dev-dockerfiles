089436b1ec0c9bc85f3c0802d0915e265fa5efc9
Dimitrios Desyllas <pc-magas@users.noreply.github.com>
2019-06-14 15:43:57 +0300

    php-fpm launches

--- entrypoint/develop_entrypoint.sh
+++ entrypoint/develop_entrypoint.sh
@@ -3,7 +3,7 @@
 USER_ID=$(id -u developer)
 GROUP_ID=$(id -g developer)
 
-echo "Setting the correct user and group id for shell use \n"
+echo "Setting the correct user and group id for shell use"
 if [ ${DOCKER_UID} != ${USER_ID} ]; then
   usermod -u ${DOCKER_UID} developer
 fi
@@ -12,7 +12,7 @@ if [ ${DOCKER_GID} != ${GROUP_ID} ]; then
   groupmod -g ${DOCKER_GID} developer
 fi
 
-echo "Setup xdebug\n"
+echo "Setup xdebug"
 cp ${XDEBUG_CONF_FILE}.orig ${XDEBUG_CONF_FILE}
 
 if [ -z ${XDEBUG_HOST} ]; then
@@ -23,13 +23,13 @@ fi
 echo "xdebug.remote_host=${XDEBUG_HOST}" >> ${XDEBUG_CONF_FILE}
 echo "xdebug.repomote_port=${XDEBUG_PORT}" >> ${XDEBUG_CONF_FILE}
 
-if [XDEBUG_DBGP = TRUE]; then
+if [ XDEBUG_DBGP = TRUE ]; then
     echo "xdebug.remote.handler=dbgp" >> ${XDEBUG_CONF_FILE}
 fi
 
-if [! -z "${XDEBUG_IDE_KEY}"]; then
+if [ ! -z "${XDEBUG_IDE_KEY}" ]; then
     echo "xdebug.idekey=\"${XDEBUG_IDE_KEY}\"" >> ${XDEBUG_CONF_FILE}
 fi
 
-echo "Launch application\n"
-exec "$@"
+echo "Launch application"
+exec $@
